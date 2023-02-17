81ccdc2a1e2caa5e56974e8a3988c8e9f8d5159c
Ahmant <39812462+Ahmant@users.noreply.github.com>
2023-02-17 20:15:04 +0200

    Update MyFatoorahPaymentController.php

--- app/Http/Controllers/MyFatoorahPaymentController.php
+++ app/Http/Controllers/MyFatoorahPaymentController.php
@@ -8,10 +8,12 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use MyFatoorah\Library\API\Payment\MyFatoorahPayment;
+use MyFatoorah\Library\API\Payment\MyFatoorahPaymentStatus;
 
 class MyFatoorahPaymentController extends Controller
 {
     protected $myFatoorah;
+    protected $myFatoorahConfig;
 
     /**
      * create MyFatoorah object
@@ -24,14 +26,16 @@ class MyFatoorahPaymentController extends Controller
         
         abort_if(!$myFatoorahConfig || !filter_var($myFatoorahConfig['status'], FILTER_VALIDATE_BOOLEAN), 403);
 
-        $this->myFatoorah = new MyFatoorahPayment([
+        $this->myFatoorahConfig = [
             'apiKey' => $myFatoorahConfig['api_key'],
             'countryCode' => $myFatoorahConfig['country_code'],
             'isTest' => filter_var($myFatoorahConfig['is_test'], FILTER_VALIDATE_BOOLEAN),
             // 'apiKey' => 'rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL',
             // 'countryCode' => 'KWT',
             // 'isTest' => true,
-        ]);
+        ];
+
+        $this->myFatoorah = new MyFatoorahPayment($this->myFatoorahConfig);
     }
 
     /**
@@ -92,7 +96,9 @@ class MyFatoorahPaymentController extends Controller
     public function callback()
     {
         try {
-            $data = $this->myFatoorah->getPaymentStatus(request('paymentId'), 'PaymentId');
+            $myFatoorahPayment = new MyFatoorahPaymentStatus($this->myFatoorahConfig);
+
+            $data = $myFatoorahPayment->getPaymentStatus(request('paymentId'), 'PaymentId');
 
             if ($data->InvoiceStatus == 'Paid') {
                 $msg = 'Invoice is paid.';
