8278c310e7d73aef47fece1807fba78e6d875519
Ahmant <39812462+Ahmant@users.noreply.github.com>
2023-02-17 17:39:32 +0200

    [Order] Add "guarded" fields

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $guarded = [];
+    
     protected $casts = [
         'order_amount' => 'float',
         'coupon_discount_amount' => 'float',
