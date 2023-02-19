1aaf1deb0d568cbc11a39eeef5d8e647d583d29b
Ahmant <39812462+Ahmant@users.noreply.github.com>
2023-02-19 07:55:24 +0200

    Remove "RemoveDoubleSlashes" middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,7 +24,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Fruitcake\Cors\HandleCors::class,
-        \App\Http\Middleware\RemoveDoubleSlashes::class,
     ];
 
     /**
