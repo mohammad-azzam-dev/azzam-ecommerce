b8970a7a7337efc3a75dc8403b0d09a6bfe2b91a
Karim Jammoul <karimjammoul9@gmail.com>
2023-03-12 20:02:52 +0200

    test

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -156,14 +156,14 @@ class OrderController extends Controller
             }
 
             // send whatsapp message
-//            try {
-//                $twilioService = new TwilioService();
-//                $message = "New order received: Order #{$o_id} - From: {$request->user()->f_name} {$request->user()->l_name}";
-//
-//                $twilioService->sendWhatsAppMessage("+96171739279", $message);
-//            } catch (\Exception $exception) {
-//                dd($exception->getMessage());
-//            }
+            try {
+                $twilioService = new TwilioService();
+                $message = "New order received: Order #{$o_id} - From: {$request->user()->f_name} {$request->user()->l_name}";
+
+                $twilioService->sendWhatsAppMessage("+96171739279", $message);
+            } catch (\Exception $exception) {
+                dd($exception->getMessage());
+            }
 
             return response()->json([
                 'message' => 'Order placed successfully!',
